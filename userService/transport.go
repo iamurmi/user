a13e032280b180dff9d9846da3ebedb02a6c94dd
Urmila Kewat <urmilakewat@ayoconnect.id>
2023-02-17 22:23:15 +0530

    calling via grpc client instead of svc layer

--- userService/transport.go
+++ userService/transport.go
@@ -38,17 +38,27 @@ func FailedResponse(data interface{}, err error) (resp ResponseWrap) {
 
 // End of RESP Wrapper
 
+type Routes struct {
+	userClient userPB.UserClient
+}
+
+func NewUserRoutes(userClient userPB.UserClient) *Routes {
+	return &Routes{
+		userClient: userClient,
+	}
+}
+
 // Handler Apis call
-func NewRoutes(e *gin.Engine, svcObj contracts.UserSeriveContract) { // Initializer of Transport layer
-	e.POST("/tusi/api/smallmic/adduser", AddUserHandler(svcObj))
-	e.GET("/tusi/api/smallmic/getuser/:id", GetUserHandler(svcObj))
-	e.GET("/tusi/api/smallmic/getusers", GetUsersHandler(svcObj))
+func (r *Routes) NewRoutes(e *gin.Engine, svcObj contracts.UserSeriveContract) { // Initializer of Transport layer
+	e.POST("/tusi/api/smallmic/adduser", r.AddUserHandler(svcObj))
+	e.GET("/tusi/api/smallmic/getuser/:id", r.GetUserHandler(svcObj))
+	e.GET("/tusi/api/smallmic/getusers", r.GetUsersHandler(svcObj))
 }
 
 // Handler Functions
 // When we want to call this function we doesnt have to pass context, but we need the context for decoding json to struct, so here i create an anonymous function who take context as arguments which doest not required to pass
 
-func AddUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
+func (r *Routes) AddUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		var user domain.User
 		err := c.ShouldBindJSON(&user) // postman request stored inside of c (gin context).
@@ -67,7 +77,8 @@ func AddUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 			},
 		}
 		//Svc call
-		id, err := svc.AddUser(c, &userPb)
+		// id, err := svc.AddUser(c, &userPb)
+		id, err := r.userClient.AddUser(c, &userPb) // GRPC Client
 		if err != nil {
 			failedStructWrapper := FailedResponse(nil, err)
 			c.JSON(400, failedStructWrapper)
@@ -78,14 +89,15 @@ func AddUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 	}
 }
 
-func GetUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
+func (r *Routes) GetUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		//Svc call
 		req := userPB.GetUserRequest{
 			Id: id,
 		}
-		resp, err := svc.GetUser(c, &req)
+		// resp, err := svc.GetUser(c, &req)
+		resp, err := r.userClient.GetUser(c, &req) // GRPC Client
 		if err != nil {
 			failedStructWrapper := FailedResponse(nil, err)
 			c.JSON(400, failedStructWrapper)
@@ -101,10 +113,13 @@ func GetUserHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 	}
 }
 
-func GetUsersHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
+func (r *Routes) GetUsersHandler(svc contracts.UserSeriveContract) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		//Svc call
-		resp, err := svc.GetUsers(c)
+		// resp, err := svc.GetUsers(c)
+
+		var emptyObj *userPB.ListUsersRequest
+		resp, err := r.userClient.GetUsers(c, emptyObj)
 		if err != nil {
 			failedStructWrapper := FailedResponse(nil, err)
 			c.JSON(400, failedStructWrapper)
